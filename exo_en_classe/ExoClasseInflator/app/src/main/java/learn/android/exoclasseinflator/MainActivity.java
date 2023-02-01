9a55d0afde47134000984c75f43f8d3e242111af
jflidou <jean-francois.lidou@isi-mtl.com>
2023-02-01 12:59:49 -0500

    addd comment

--- exo_en_classe/ExoClasseInflator/app/src/main/java/learn/android/exoclasseinflator/MainActivity.java
+++ exo_en_classe/ExoClasseInflator/app/src/main/java/learn/android/exoclasseinflator/MainActivity.java
@@ -24,15 +24,20 @@ public class MainActivity extends AppCompatActivity {
         btnInflate = findViewById(R.id.btn_inflate);
         llListStudent = findViewById(R.id.ll_list_student);
         btnInflate.setOnClickListener((view) -> {
+            //deserialisé la vue de l etudiant
             LinearLayout llStudent = (LinearLayout) LayoutInflater.from(context).inflate(R.layout.student_layout, null);
+            //aller chercher les differents elements de la vue a modifier
             TextView tvNumero = llStudent.findViewById(R.id.tv_number_student_layout);
             TextView tvFirstName = llStudent.findViewById(R.id.tv_firstname_student_layout);
             TextView tvLastName = llStudent.findViewById(R.id.tv_lastname_student_layout);
+            //recuperer le prochain etudiant a afficher
             Student studentToAdd = StudentManager.getNext();
             if (studentToAdd != null) {
+                //affecter les valeurs de l etudiant dans la vue
                 tvNumero.setText("" + studentToAdd.getNumber());
                 tvFirstName.setText(studentToAdd.getFirstName());
                 tvLastName.setText(studentToAdd.getLastName());
+                //ajoute la vue au liniear layout qui contient la liste de etudiant.
                 llListStudent.addView(llStudent);
             }
         });
