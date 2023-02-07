9d442ae5a9884fedf5464e5c3ce91b20e1601af2
jflidou <jean-francois.lidou@isi-mtl.com>
2023-02-07 12:13:38 -0500

    start project exo class view combinaison

---
+++ exo_en_classe_correction/ExoClasseViewCombinaison/app/src/main/java/learn/android/exoclasseviewcombinaison/MainActivity.java
@@ -0,0 +1,15 @@
+package learn.android.exoclasseviewcombinaison;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+public class MainActivity extends AppCompatActivity {
+    LinearLayout llMain;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
