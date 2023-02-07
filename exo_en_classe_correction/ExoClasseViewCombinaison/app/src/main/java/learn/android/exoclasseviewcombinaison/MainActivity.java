7721472ea4843a6ec1a3b04cb97b3a95448362c1
jflidou <jean-francois.lidou@isi-mtl.com>
2023-02-07 13:05:31 -0500

    correction exo class view combinaison

--- exo_en_classe_correction/ExoClasseViewCombinaison/app/src/main/java/learn/android/exoclasseviewcombinaison/MainActivity.java
+++ exo_en_classe_correction/ExoClasseViewCombinaison/app/src/main/java/learn/android/exoclasseviewcombinaison/MainActivity.java
@@ -4,12 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+
+import learn.android.exoclasseviewcombinaison.view.VignetteView;
 public class MainActivity extends AppCompatActivity {
     LinearLayout llMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        llMain = findViewById(R.id.ll_main_act);
+        VignetteView vignetteView1 = new VignetteView(this);
+        vignetteView1.setLabel("Test 1");
+        vignetteView1.setImage(R.drawable.personne1);
+        VignetteView vignetteView2 = new VignetteView(this);
+        vignetteView2.setLabel("Test 12");
+        vignetteView2.setImage(R.drawable.personne2);
+        llMain.addView(vignetteView1);
+        llMain.addView(vignetteView2);
     }
 }
---
+++ exo_en_classe_correction/ExoClasseViewCombinaison/app/src/main/java/learn/android/exoclasseviewcombinaison/view/VignetteView.java
@@ -0,0 +1,50 @@
+package learn.android.exoclasseviewcombinaison.view;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import learn.android.exoclasseviewcombinaison.R;
+public class VignetteView extends RelativeLayout {
+    TextView label;
+    ImageView image;
+    //constructeurr java
+    public VignetteView(Context context) {
+        super(context);
+        init();
+    }
+    private void init() {
+        //creer vur programmatique
+//        label = new TextView(getContext());
+//        image = new ImageView(getContext());
+//        addView(image);
+//        addView(label);
+        //creer ma vue depuis le XML
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.vignette_view_layout, this);
+        label = view.findViewById(R.id.tv_vignette_layout);
+        image = view.findViewById(R.id.img_vignette_layout);
+    }
+    public VignetteView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+        TypedArray customAttributes = getContext().obtainStyledAttributes(attrs, R.styleable.VignetteView);
+        String labelValue = customAttributes.getString(R.styleable.VignetteView_label);
+        int idImg = customAttributes.getResourceId(R.styleable.VignetteView_id_ressource_img, R.drawable.personne4);
+        if (labelValue == null)
+            labelValue = "No set";
+        setLabel(labelValue);
+        setImage(idImg);
+        customAttributes.recycle();
+    }
+    //methode public
+    public void setLabel(String label) {
+        this.label.setText(label);
+    }
+    public void setImage(int idResource) {
+        image.setImageResource(idResource);
+    }
+}
