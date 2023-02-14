f1f151f7b77965e6691a14f19bc24586ffd7614a
jflidou <jean-francois.lidou@isi-mtl.com>
2023-02-14 12:16:45 -0500

    demo direction event

--- demo/DemoEventTouch/app/src/main/java/learn/android/demoeventtouch/MainActivity.java
+++ demo/DemoEventTouch/app/src/main/java/learn/android/demoeventtouch/MainActivity.java
@@ -2,6 +2,8 @@ package learn.android.demoeventtouch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -9,17 +11,21 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private EditText edEventWatcher;
-    private TextView tvPositionEtat ;
+    private TextView tvPositionEtat;
     private String TAG = "debug_app";
+    private LinearLayout llEventDirection;
+    PointF pointDown, pointUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edEventWatcher = findViewById(R.id.ed_event_watcher);
         tvPositionEtat = findViewById(R.id.tv_event_on_touch);
+        llEventDirection = findViewById(R.id.ll_event_direction);
         edEventWatcher.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -39,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
                 edEventWatcher.setTextColor(colorText);
             }
         });
+        pointUp = new PointF();
+        pointDown = new PointF();
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
@@ -49,19 +57,40 @@ public class MainActivity extends AppCompatActivity {
         StringBuffer msg = new StringBuffer();
         msg.append("position x :");
         if (actionMasked == MotionEvent.ACTION_DOWN) {
-            Log.d(TAG, "onTouchEvent: down");
+//            Log.d(TAG, "onTouchEvent: down");
             tvPositionEtat.setBackgroundColor(Color.YELLOW);
             msg.append(xPosition + " y: " + yPosition);
+            pointDown.set(xPosition, yPosition);
         }
         if (actionMasked == MotionEvent.ACTION_MOVE) {
-            Log.d(TAG, "onTouchEvent: move");
+//            Log.d(TAG, "onTouchEvent: move");
             tvPositionEtat.setBackgroundColor(Color.BLUE);
             msg.append(xPosition + " y: " + yPosition);
         }
         if (actionMasked == MotionEvent.ACTION_UP) {
-            Log.d(TAG, "onTouchEvent: up");
+//            Log.d(TAG, "onTouchEvent: up");
             tvPositionEtat.setBackgroundColor(Color.RED);
             msg.append(xPosition + " y: " + yPosition);
+            pointUp.set(xPosition, yPosition);
+            float dx = pointDown.x - pointUp.x;
+            float dy = pointDown.y - pointUp.y;
+            if (Math.abs(dx) > Math.abs(dy)) {
+                Log.d(TAG, "onTouchEvent: gauche droite");
+                //si droite
+                if(dx > 0){
+                    llEventDirection.setBackgroundColor(Color.BLUE);
+                }else {
+                    //si gauche
+                    llEventDirection.setBackgroundColor(Color.RED);
+                }
+            } else {
+                Log.d(TAG, "onTouchEvent: haut bas");
+                if(dy > 0){
+                    llEventDirection.setBackgroundColor(Color.GREEN);
+                }else{
+                    llEventDirection.setBackgroundColor(Color.YELLOW);
+                }
+            }
         }
         tvPositionEtat.setText(msg.toString());
         return true;
