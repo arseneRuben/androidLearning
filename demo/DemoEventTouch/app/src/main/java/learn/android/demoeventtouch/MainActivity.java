a1f021a1da352a3694e459dbf4c1ca4a4a60f142
jflidou <jean-francois.lidou@isi-mtl.com>
2023-02-14 11:42:04 -0500

    demo event

--- demo/DemoEventTouch/app/src/main/java/learn/android/demoeventtouch/MainActivity.java
+++ demo/DemoEventTouch/app/src/main/java/learn/android/demoeventtouch/MainActivity.java
@@ -6,16 +6,20 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-    EditText edEventWatcher;
+    private EditText edEventWatcher;
+    private TextView tvPositionEtat ;
     private String TAG = "debug_app";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edEventWatcher = findViewById(R.id.ed_event_watcher);
+        tvPositionEtat = findViewById(R.id.tv_event_on_touch);
         edEventWatcher.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -35,5 +39,31 @@ public class MainActivity extends AppCompatActivity {
                 edEventWatcher.setTextColor(colorText);
             }
         });
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int actionMasked = event.getActionMasked();
+        tvPositionEtat.setTextColor(Color.WHITE);
+        float xPosition = event.getX();
+        float yPosition = event.getY();
+        StringBuffer msg = new StringBuffer();
+        msg.append("position x :");
+        if (actionMasked == MotionEvent.ACTION_DOWN) {
+            Log.d(TAG, "onTouchEvent: down");
+            tvPositionEtat.setBackgroundColor(Color.YELLOW);
+            msg.append(xPosition + " y: " + yPosition);
+        }
+        if (actionMasked == MotionEvent.ACTION_MOVE) {
+            Log.d(TAG, "onTouchEvent: move");
+            tvPositionEtat.setBackgroundColor(Color.BLUE);
+            msg.append(xPosition + " y: " + yPosition);
+        }
+        if (actionMasked == MotionEvent.ACTION_UP) {
+            Log.d(TAG, "onTouchEvent: up");
+            tvPositionEtat.setBackgroundColor(Color.RED);
+            msg.append(xPosition + " y: " + yPosition);
+        }
+        tvPositionEtat.setText(msg.toString());
+        return true;
     }
 }
