58a4f9b26c4596f333d5c8506cab0b7e37f4dded
jflidou <jean-francois.lidou@isi-mtl.com>
2023-02-20 10:34:50 -0500

    add power point listview

---
+++ exo_en_classe_correction/ExoClasseListView/app/src/main/java/learn/android/exoclasselistview/MainActivity.java
@@ -0,0 +1,11 @@
+package learn.android.exoclasselistview;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
